e5c95a59183163e481893f30b445f9121687d02b
daridev <hernandezdarifrancisco@gmail.com>
2022-05-04 15:56:08 -0500

    social layout and responsive

--- code/header.js
+++ code/header.js
@@ -3,7 +3,7 @@ const active_menu_option = document.querySelector ("header nav ul li a.active")
 active_menu_option.setAttribute ("href", "#")
 
 // Active or deactive menu
-const menu_btn = document.querySelector ("header .btn-menu")
+const menu_btn = document.querySelector ("header .btn.menu")
 const nav = document.querySelector ("header nav")
 menu_btn.addEventListener ("click", function (e) {
     menu_btn.classList.toggle ("active")
