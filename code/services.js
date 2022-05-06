3ab90e7ae630392659b3674891cd25a48b6bea33
daridev <hernandezdarifrancisco@gmail.com>
2022-05-06 16:25:16 -0500

    update services to grid

--- code/services.js
+++ code/services.js
@@ -1,9 +1,8 @@
-const links = document.querySelectorAll (".service .paralax")
+const links = document.querySelectorAll ("a.service")
 
 for (let link of links) {
     // Generate whatsapp link
-    let service = link.parentNode
-    let service_title = service.querySelector("h2").innerHTML
+    let service_title = link.querySelector("h2").innerHTML
     let message = "Me interesa su servicio de " + service_title
     let message_formated = message.replaceAll(" ", "%20")
     let whatsapp_link = "https://api.whatsapp.com/send?phone=5219982391978&text=" + message_formated
