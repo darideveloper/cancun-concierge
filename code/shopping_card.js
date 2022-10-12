8b546e09a0a606272919350e6c500b2375db1960
daridev <hernandezdarifrancisco@gmail.com>
2022-10-12 16:41:58 -0500

    manage shopping form events

---
+++ code/shopping_card.js
@@ -0,0 +1,67 @@
+// get buy forms
+const buy_forms = document.querySelectorAll ("article.service .buy form")
+
+// Lements in the shopping cart
+let cart_elems = []
+
+function render_cart () {
+    // TODO: Render again card in the html of the page
+    console.log ("cart rendered")
+}
+
+function manage_froms () {
+    // Manage form event to buy services or add to cart
+    
+    // Loop for each uy form
+    for (const buy_form of buy_forms) {
+
+        // form status
+        let date_selected = ""
+    
+        // Add listener to checkbox for update date_selected
+        const checkboxes = buy_form.querySelectorAll ("label > input")
+        for (const checkbox of checkboxes) {
+            checkbox.addEventListener ("click", (e) => {
+                date_selected = e.target.value
+            })
+        }
+    
+    
+    
+        buy_form.addEventListener ("submit", (e) => {
+            // Disable submit events in each form
+            e.preventDefault ()
+        })
+        
+        // Add event listeners for form buttons
+        const buy_button = buy_form.querySelector ("button.buy-now")
+        const add_button = buy_form.querySelector ("button.add-cart")
+    
+        buy_button.addEventListener ("click", () => {
+            if (date_selected) {
+                // TODO: Submit to payments platform
+                console.log ("to payments")
+            }
+        })
+    
+        add_button.addEventListener ("click", () => {
+            if (date_selected) {
+                
+                // Get service
+                const service_date = `Feb ${date_selected}`
+                let service_name = buy_form.parentNode.parentNode.parentNode.querySelector ("h3").innerHTML
+                service_name = service_name.replace ("&amp;", "&")
+                const service = `${service_date} - ${service_name}`
+    
+                // save service
+                cart_elems.push (service)
+
+                // Update cart
+                render_cart()
+    
+            }
+        })    
+    }
+}
+
+manage_froms ()
